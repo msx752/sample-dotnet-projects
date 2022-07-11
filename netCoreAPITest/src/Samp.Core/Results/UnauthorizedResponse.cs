663f94f02edf4c2fd4be67c49d91976acd885870
Mustafa Salih ASLIM <salih@aslim.net>
2022-07-12 00:44:04 +0300

    update3

---
+++ netCoreAPITest/src/Samp.Core/Results/UnauthorizedResponse.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Samp.Core.Results.Abstracts;
+using System.Collections.Generic;
+
+namespace Samp.Core.Results
+{
+    public sealed class UnauthorizedResponse
+        : BaseResult
+    {
+        public UnauthorizedResponse()
+            : base(StatusCodes.Status401Unauthorized)
+        {
+        }
+
+        public UnauthorizedResponse(string message)
+            : base(StatusCodes.Status401Unauthorized, message)
+        {
+        }
+
+        public UnauthorizedResponse(IEnumerable<string> messages)
+            : base(StatusCodes.Status401Unauthorized, messages)
+        {
+        }
+    }
+}

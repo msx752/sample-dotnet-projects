7494da8c933119feda8335580909a86e2e8ff3a5
Mustafa Salih ASLIM <salih@aslim.net>
2022-07-15 17:40:33 +0300

    bug fix

--- netCoreAPITest/src/Samp.Core/Extensions/ContextSeedExtensions.cs
+++ netCoreAPITest/src/Samp.Core/Extensions/ContextSeedExtensions.cs
@@ -22,7 +22,10 @@ namespace Samp.Core.Extensions
 
             foreach (var item in contextSeeds)
             {
-                if (item != null && !typeof(IContextSeed).IsAssignableFrom(item))
+                if (item == null)
+                    continue;
+
+                if (!typeof(IContextSeed).IsAssignableFrom(item))
                 {
                     throw new Exception($"{item.FullName} must implement {typeof(ContextSeed).FullName}");
                 }

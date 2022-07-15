07985b3a5497bcb7837fd150a5b9686ff065fefb
Mustafa Salih ASLIM <salih@aslim.net>
2022-07-15 19:00:14 +0300

    bug fix
    (cherry picked from commit a5386378a32646303bbe8984f82234d97b2560a6)

--- netCoreAPITest/src/MovieService/Samp.Movie.API/Models/Dtos/RatingDto.cs
+++ netCoreAPITest/src/MovieService/Samp.Movie.API/Models/Dtos/RatingDto.cs
@@ -2,7 +2,7 @@
 {
     public class RatingDto
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public double AverageRating { get; set; }
         public int NumVotes { get; set; }
         public string MovieId { get; set; }
